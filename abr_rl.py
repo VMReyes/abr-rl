2baa11be553426223a8b439658baa4074fe32bfc
Victor Reyes <vmreyes@mit.edu>
2021-10-22 14:59:25 -0400

    parsed data into structure

--- abr_rl.py
+++ abr_rl.py
@@ -1,4 +1,24 @@
 import d3rlpy
+import numpy as np
+
 
 if __name__ == "__main__":
-    pass
+    logged_data = np.load("data/mpc_traj.npy")
+    states = []
+    actions = []
+    rewards = []
+    terminals = []
+    for session in logged_data:
+        for step in session:
+            states.append(np.array(step[0:18]))
+            actions.append(np.array(step[18]))
+            rewards.append(np.array(step[19]))
+            terminals.append(np.array(step[-1]))
+    states = np.array(states)
+    actions = np.array(actions).astype(int)
+    print(actions)
+    rewards = np.array(rewards)
+    terminals = np.array(terminals)
+    dataset = d3rlpy.dataset.MDPDataset(states, actions, rewards, terminals)
+    bcq = d3rlpy.algos.BCQ()
+    bcq.fit(dataset, n_steps=1000)
