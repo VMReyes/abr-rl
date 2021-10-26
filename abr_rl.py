69b3f563a8afa1d0ed56322a25c6aaa013ba9950
Victor Reyes <vmreyes@mit.edu>
2021-10-26 17:25:34 -0400

    file trains and loads all trajectories

--- abr_rl.py
+++ abr_rl.py
@@ -1,24 +1,31 @@
 import d3rlpy
 import numpy as np
 
+LOGGED_DATA_FILES = ["data/bba_traj.npy",
+                     "data/bola_traj.npy",
+                     "data/mpc_traj.npy",
+                     "data/opt_rate_traj.npy",
+                     "data/pess_rate_traj.npy",
+                     "data/rate_traj.npy"]
 
 if __name__ == "__main__":
-    logged_data = np.load("data/mpc_traj.npy")
     states = []
     actions = []
     rewards = []
     terminals = []
-    for session in logged_data:
-        for step in session:
-            states.append(np.array(step[0:18]))
-            actions.append(np.array(step[18]))
-            rewards.append(np.array(step[19]))
-            terminals.append(np.array(step[-1]))
+    for data_filename in LOGGED_DATA_FILES:
+        print(f'Loading {data_filename}.')
+        logged_data = np.load(data_filename)
+        for session in logged_data:
+            for step in session:
+                states.append(np.array(step[0:19]))
+                actions.append(np.array(step[19]))
+                rewards.append(np.array(step[20]))
+                terminals.append(np.array(step[-1]))
     states = np.array(states)
     actions = np.array(actions).astype(int)
-    print(actions)
     rewards = np.array(rewards)
     terminals = np.array(terminals)
     dataset = d3rlpy.dataset.MDPDataset(states, actions, rewards, terminals)
-    bcq = d3rlpy.algos.BCQ()
-    bcq.fit(dataset, n_steps=1000)
+    bcq = d3rlpy.algos.DiscreteBCQ()
+    bcq.fit(dataset, n_epochs=2)
