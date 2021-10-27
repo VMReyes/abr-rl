e07ea9a2a58921557337618b2be182d4a94238d5
Victor Reyes <vmreyes@mit.edu>
2021-10-26 20:56:13 -0400

    added readout of best seen trajectory

--- abr_rl.py
+++ abr_rl.py
@@ -14,15 +14,25 @@ def get_mdp_dataset_from_datafiles():
     actions = []
     rewards = []
     terminals = []
+    best_session_score = 0
+    best_session_rewards = []
     for data_filename in LOGGED_DATA_FILES:
         print(f'Loading {data_filename}.')
         logged_data = np.load(data_filename)
         for session in logged_data:
+            session_rewards = []
             for step in session:
                 states.append(np.array(step[0:19]))
                 actions.append(np.array(step[19]))
                 rewards.append(np.array(step[20]))
+                session_rewards.append(np.array(step[20]))
                 terminals.append(np.array(step[-1]))
+            if np.sum(session_rewards) > best_session_score:
+                best_session_rewards = session_rewards.copy()
+                best_session_score = np.sum(session_rewards)
+    print(f'Best seen session cumulative reward: {np.sum(best_session_rewards)}.')
+    print(f'Best seen session average reward: {np.average(best_session_rewards)}.')
+
     states = np.array(states)
     actions = np.array(actions).astype(int)
     rewards = np.array(rewards)
@@ -34,4 +44,4 @@ def get_mdp_dataset_from_datafiles():
 if __name__ == "__main__":
     dataset = get_mdp_dataset_from_datafiles()
     bcq = d3rlpy.algos.DiscreteBCQ()
-    bcq.fit(dataset, n_epochs=2)
+    bcq.fit(dataset, n_epochs=1)
