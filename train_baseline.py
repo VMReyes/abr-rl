ea04c207305c9f343eeeb1016f135ba567100b15
Victor Reyes <vmreyes@mit.edu>
2021-11-23 18:03:36 -0500

    added script to train baseline a2c

---
+++ train_baseline.py
@@ -0,0 +1,32 @@
+import gym, ray
+import ray.rllib.agents.a3c as a3c
+from ray.tune.logger import pretty_print
+import numpy as np
+from env.abr import ABRSimEnv
+from gym.spaces import Discrete, Box
+
+class myEnv(gym.Env):
+    def __init__(self, env_config):
+        # Launch ABR environment
+        print('Setting up environment..')
+        self.env = ABRSimEnv()
+        self.action_space = Discrete(6)
+        self.observation_space = Box(low=0, high=np.inf, shape=(19,), dtype=np.float32)
+
+    def reset(self):
+        initial_obs, initial_obs_extended = self.env.reset()
+        return initial_obs
+    def step(self, action):
+        obs, reward, done, info = self.env.step(action)
+        return obs, reward, done, info
+
+if __name__ == "__main__":
+    ray.init()
+    config = a3c.DEFAULT_CONFIG.copy()
+    config["num_gpus"] = 0
+    config["num_workers"] = 1
+    trainer = a3c.A2CTrainer(config=config, env=myEnv)
+    for i in range(100):
+        result = trainer.train()
+        print(pretty_print(result))
+
