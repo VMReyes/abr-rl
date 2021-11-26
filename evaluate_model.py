6d0b73977064e3d730297d275883012d79c90772
Howard DaCosta <howard.dacosta400@gmail.com>
2021-11-26 12:26:21 -0500

    added average trajectory graphs

--- evaluate_model.py
+++ evaluate_model.py
@@ -5,7 +5,7 @@ import csv
 from env.abr import ABRSimEnv
 import matplotlib.pyplot as plt
 
-#MODEL_WEIGHTS = "d3rlpy_logs/DiscreteCQL_20211115220329/model_8950.pt"
+# MODEL_WEIGHTS = "d3rlpy_logs/DiscreteCQL_20211115220329/model_8950.pt"
 MODEL_WEIGHTS = "d3rlpy_logs/DiscreteBCQ_20211115222009/model_8950.pt"
 
 def evaluate_agent(agent):
@@ -16,7 +16,7 @@ def evaluate_agent(agent):
     # Number of traces we intend to run through, more gives us a better evaluation
     num_traces = 25
 
-    trajectories = [] 
+    trajectories = []
 
     for _ in range(num_traces):
         # Done in reset: Randomly choose a trace and starting point in it
@@ -24,7 +24,7 @@ def evaluate_agent(agent):
         done = False
         t = 0
         rews = []
-        metrics = {"action": [], "buffer_length": [], "action_bitrate": []}
+        metrics = {"action": [], "buffer_length": [], "action_bitrate": [], "file_size": []}
         while not done:
             # Choose an action through random policy
             act = agent.predict(obs[np.newaxis, :]).item()
@@ -65,12 +65,46 @@ def plot_trajectory(trajectory):
     plt.tight_layout(pad=1.0)
     plt.savefig("output/latest_trajectory_bcq.png")
 
+def plot_avg_trajectory_metrics(trajectories):
+    num_trajectories = len(trajectories)
+    avg_buffer_length = [0 for _ in range(490)]
+    avg_bitrate_history = [0 for _ in range(490)]
+    
+    for t in trajectories:
+        buffer_history = t["buffer_length"]
+        for i in range(len(buffer_history)):
+            avg_buffer_length[i] += buffer_history[i]
+        action_bitrate_history = t["action_bitrate"]
+        for i in range(len(buffer_history)):
+            avg_bitrate_history[i] += action_bitrate_history[i]
+    
+    for i in range(490):
+        avg_buffer_length[i] /= num_trajectories
+        avg_bitrate_history[i] /= num_trajectories
+    
+
+    plt.subplot(211)
+    plt.plot(avg_buffer_length)
+    plt.title("Average Trajectory Buffer Length over Time")
+
+    plt.subplot(212)
+    plt.plot(avg_bitrate_history)
+    plt.title("Average Trajectory Bitrate Choice over Time")
+
+    plt.tight_layout(pad=1.0)
+    plt.savefig("output/average_trajectory_cql.png")
+    
+
+
+
+
 
 if __name__ == "__main__":
-    #model = d3rlpy.algos.DiscreteCQL.from_json("d3rlpy_logs/DiscreteCQL_20211115220329/params.json")
+    # model = d3rlpy.algos.DiscreteCQL.from_json("d3rlpy_logs/DiscreteCQL_20211115220329/params.json")
     model = d3rlpy.algos.DiscreteBCQ.from_json("d3rlpy_logs/DiscreteBCQ_20211115222009/params.json")
     model.load_model(MODEL_WEIGHTS)
     trajectories = evaluate_agent(model)
     plot_trajectory(trajectories[0])
+    # plot_avg_trajectory_metrics(trajectories)
 
 
