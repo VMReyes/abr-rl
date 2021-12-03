8ec5f3aa8a154db1af2305a9983360616411e30f
Victor M Reyes Espinoza <vmreyes@visiongpu11.csail.mit.edu>
2021-12-03 15:03:13 -0500

    added changes for ppo

--- train_baseline.py
+++ train_baseline.py
@@ -1,5 +1,6 @@
 import gym, ray
 import ray.rllib.agents.a3c as a3c
+import ray.rllib.agents.ppo as ppo
 from ray.tune.logger import pretty_print
 import numpy as np
 from env.abr import ABRSimEnv
@@ -18,19 +19,31 @@ class myEnv(gym.Env):
         return initial_obs
     def step(self, action):
         obs, reward, done, info = self.env.step(action)
+        obs /= np.array([1e6, 1e6, 1e6, 1e6, 1e6,
+                         1, 1, 1, 1, 1,
+                         40, 490, 6,
+                         1e6, 1e6, 1e6, 1e6, 1e6, 1e6])
         return obs, reward, done, info
 
 if __name__ == "__main__":
     ray.init()
-    config = a3c.DEFAULT_CONFIG.copy()
+    #config = a3c.DEFAULT_CONFIG.copy()
+    config = ppo.DEFAULT_CONFIG.copy()
     config["num_gpus"] = 0
     config["num_workers"] = 1
     config["lr"] = 1e-2
     config["lambda"] = 0.96
     config["gamma"] = 0.96
+    config["entropy_coeff_schedule"] = [(0, 0.2), (2500*490, 0.0)]
+    config["model"]["fcnet_hiddens"] = [64, 32]
+    config["model"]["fcnet_activation"] = "relu" 
+    config["rollout_fragment_length"] = 490
 
-    trainer = a3c.A2CTrainer(config=config, env=myEnv)
-    for i in range(100):
+    #trainer = a3c.A2CTrainer(config=config, env=myEnv)
+    trainer = ppo.PPOTrainer(config=config, env=myEnv)
+    for i in range(7500):
         result = trainer.train()
         print(pretty_print(result))
-
+        if i % 100 == 0:
+            checkpoint = trainer.save("output/1e2")
+            print("checkpoint saved at", checkpoint)
